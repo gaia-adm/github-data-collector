01b238fb2850ea20f5912bb8b691ad4ac5726a32
Effi <effib@hp.com>
2016-05-06 22:43:29 +0300

    fetch commit data

--- main.go
+++ main.go
@@ -9,48 +9,58 @@ import (
 	"strings"
 )
 
-type Message struct {
-	comments_url string
-}
-
 func main() {
 
-	log.Info(getRepositoriesCommitUrls())
+	for _, currRepository := range getRepositories() {
+		log.Infof("Repository: %s, Commits: %d", currRepository["name"], len(getCommits(currRepository)))
+	}
 }
 
-func getRepositories() []byte {
+func getCommits(repository map[string]interface{}) []map[string]interface{} {
 
-	response, err := http.Get("https://api.github.com/orgs/gaia-adm/repos?per_page=1000")
+	return toMap(httpGet(getCommitsUrl(repository)))
+}
+
+func toMap(data []byte) []map[string]interface{} {
+
+	var ret []map[string]interface{}
+	json.Unmarshal(data, &ret)
+
+	return ret
+}
+
+func getCommitsUrl(repository map[string]interface{}) string {
+
+	var ret string
+	commitsUrl := repository["commits_url"]
+	if typeof(commitsUrl) == "string" {
+		currUrl := commitsUrl.(string)
+		if len(currUrl) > 0 && strings.HasSuffix(currUrl, "/commits{/sha}") {
+			ret = strings.TrimSuffix(currUrl, "{/sha}")
+		}
+	}
+
+	return ret
+}
+
+func getRepositories() []map[string]interface{} {
+
+	return toMap(httpGet("https://api.github.com/orgs/gaia-adm/repos"))
+}
+
+func httpGet(url string) []byte {
+
+	response, err := http.Get(url + "?per_page=1000")
 	if err != nil {
-		log.Error("Failed to get gaia-adm repositories from github. ", err)
 		panic(err)
 	}
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Error("Failed to read gaia-adm repositories' response JSON from github. ", err)
 		panic(err)
 	}
 
 	return body
-}
-
-func getRepositoriesCommitUrls() []string {
-
-	var ret []string
-	var repositories []map[string]interface{}
-	json.Unmarshal(getRepositories(), &repositories)
-	for _, currRepository := range repositories {
-		commitsUrl := currRepository["commits_url"]
-		if typeof(commitsUrl) == "string" {
-			currUrl := currRepository["commits_url"].(string)
-			if len(currUrl) > 0 && strings.HasSuffix(currUrl, "/commits{/sha}") {
-				ret = append(ret, strings.TrimSuffix(currUrl, "{/sha}"))
-			}
-		}
-	}
-
-	return ret
 }
 
 func typeof(v interface{}) string {
