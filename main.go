6bfedd30d91635aac55cba860213b8592366e78d
boriska70@gmail.com <boriska70@gmail.com>
2016-05-11 09:40:11 +0300

    use http_proxy envvar, if presented; monitor github api calls limits

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	"net/http"
+	"net/url"
 	"io/ioutil"
 	"encoding/json"
 	"fmt"
 	"strings"
+	"os"
+	"time"
+	"strconv"
 )
 
+var proxyURL *url.URL
+
 func main() {
+
+	proxyString, found := os.LookupEnv("http_proxy")
+	if found {
+		proxyURL, _ = url.Parse(proxyString)
+	}
 
 	for _, currRepository := range getRepositories() {
 		log.Infof("Repository: %s, Commits: %d", currRepository["name"], len(getCommits(currRepository)))
@@ -50,7 +61,14 @@ func getRepositories() []map[string]interface{} {
 
 func httpGet(url string) []byte {
 
-	response, err := http.Get(url + "?per_page=1000")
+	transport := &http.Transport{}
+	if proxyURL != nil {
+		transport.Proxy = http.ProxyURL(proxyURL)
+	}
+	client := &http.Client{Transport: transport}
+
+	response, err := client.Get(url + "?per_page=1000")
+
 	if err != nil {
 		panic(err)
 	}
@@ -58,6 +76,20 @@ func httpGet(url string) []byte {
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
+	}
+
+	/*	for k, v := range response.Header {
+			fmt.Println(k, ": ", v)
+		}*/
+
+	rlimit, _ := strconv.Atoi(response.Header.Get("X-Ratelimit-Remaining"));
+	tlimit, _ := strconv.ParseInt(response.Header.Get("X-Ratelimit-Reset"),10,64);
+	llimit, _ := strconv.Atoi(response.Header.Get("X-Ratelimit-Limit"));
+
+	if rlimit == 0 {
+		log.Infof("You're out of tries - all %d calls have been used, come after %v. Think about authorized access.", llimit, time.Unix(tlimit, 0))
+	} else {
+		log.Infof("You can ask Github another %d times during this hour", rlimit)
 	}
 
 	return body
